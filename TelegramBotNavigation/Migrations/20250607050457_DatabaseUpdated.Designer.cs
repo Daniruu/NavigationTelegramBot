466fb333bc718c18cda9c2b4708842102bf4626f
Danila Shuliakouski <danila.shuliakouski@gmail.com>
2025-06-07 07:05:41 +0200

    Add migration

--- TelegramBotNavigation/Migrations/20250607050457_DatabaseUpdated.Designer.cs
+++ TelegramBotNavigation/Migrations/20250607050457_DatabaseUpdated.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TelegramBotNavigation.Data;
@@ -11,9 +12,11 @@ using TelegramBotNavigation.Data;
 namespace TelegramBotNavigation.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250607050457_DatabaseUpdated")]
+    partial class DatabaseUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TelegramBotNavigation/Migrations/20250607050457_DatabaseUpdated.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelegramBotNavigation.Migrations
+{
+    /// <inheritdoc />
+    public partial class DatabaseUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "TelegramUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "TelegramUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
